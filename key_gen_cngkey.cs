f75f9e772367ebccf4210bfb0991c58c1c8ca5c1
Daniel Escudero <deescuderoo@unal.edu.co>
2017-09-17 17:47:49 +0200

    Create key_gen_cngkey.cs

---
+++ key_gen_cngkey.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+
+public class KeyGen
+{
+    static void Main()
+    {
+        try
+        {
+            CngKey key;
+            CngKeyCreationParameters keyparm = new CngKeyCreationParameters(); 
+            
+            // See: https://msdn.microsoft.com/en-us/library/system.security.cryptography.cngexportpolicies(v=vs.110).aspx
+            keyparm.ExportPolicy = CngExportPolicies.AllowPlaintextExport;   
+            
+            keyparm.KeyCreationOptions = CngKeyCreationOptions.OverwriteExistingKey;  //.MachineKey	.None
+            keyparm.Provider = CngProvider.MicrosoftSoftwareKeyStorageProvider;
+
+            // For different algorithms available see: https://msdn.microsoft.com/en-us/library/system.security.cryptography.cngalgorithm(v=vs.110).aspx
+            key = CngKey.Create(CngAlgorithm.ECDsaP256, "key1", keyparm);
+
+            // Export public key
+            byte[] publkey = key.Export(CngKeyBlobFormat.GenericPublicBlob);
+            String publkeybase64 = Convert.ToBase64String(publkey);
+            var currentDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+            System.IO.File.WriteAllText(currentDirectory + "pkey.txt", publkeybase64);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Error");
+        }
+    }
+}
